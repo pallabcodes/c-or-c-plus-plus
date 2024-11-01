3f262a696b984225dbe7c8361ae971135966a738
Pallab Kayal <pallabfullstack@outlook.com>
2024-11-02 00:26:05 +0530

    memory management

---
+++ pointers-related/memory_management.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * 1. Whenever a value saved, it saved in memory i.e. RAM but within which components of RAM, STACK OR HEAP (HEAP also known as dynamic memory)
+ *
+ * N.B: Heap i.e. part of RAM is difference than Heap or Binary Heap data structure, so this HEAP (from RAM) is not an implementation of Binary Heap Data structure but Stack (in RAM) is actually an implementor of STACK DATA STRUCURE
+ *
+ * 2. The memory i.e. assigned to a program or application, divided into 4 segments i) code/text ii) global iii) stack iv) heap
+ *
+ * To use dynamic memory with c++, `use new and delete`
+ *
+ */
+
+int main()
+{
+  int a; // Variable stored on the stack
+
+  int *p = nullptr; // Initialize pointer to nullptr for safety
+
+  // Allocate memory for a single integer on the heap
+  p = new int;
+  *p = 10;
+  cout << "Value at p: " << *p << endl;
+
+  // Free the allocated memory and reset pointer
+  delete p;
+  p = nullptr;
+
+  // Allocate memory for an array of 20 integers on the heap
+  p = new int[20];
+
+  // Optionally initialize array elements
+  for (int i = 0; i < 20; ++i)
+  {
+    p[i] = i + 1;
+  }
+  cout << "Array values: ";
+  for (int i = 0; i < 20; ++i)
+  {
+    cout << p[i] << " ";
+  }
+  cout << endl;
+
+  // Free the allocated memory and reset pointer
+  delete[] p;
+  p = nullptr;
+
+  return 0;
+}
+
+// resource: https://youtu.be/_8-ht2AKyH4
+// https: // www.javatpoint.com/cpp-memory-management
+// https: // www.javatpoint.com/malloc-vs-new-in-cpp
+// https: // www.javatpoint.com/free-vs-delete-in-cpp
