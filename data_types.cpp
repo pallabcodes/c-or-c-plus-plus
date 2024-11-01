cc43fb4b87814e7078d1c3e9f5f7b73e37cc3d45
Pallab Kayal <pallabfullstack@outlook.com>
2024-11-02 02:17:27 +0530

    revise

---
+++ data_types.cpp
@@ -0,0 +1,5 @@
+// https: // www.javatpoint.com/cpp-data-types
+// https: // www.javatpoint.com/cpp-keywords
+// https: // www.javatpoint.com/cpp-operators
+// https: // www.javatpoint.com/cpp-identifiers
+// https: // www.javatpoint.com/cpp-expression
